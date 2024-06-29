4f8ccd499a890184dfdeec216f3c65402ecc3ae6
kaho-cell <s1310093@u-aizu.ac.jp>
2024-06-29 16:52:18 +0900

    HoT: initial revision

---
+++ HeadorTail.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class HeadorTail{
+    public static void main(String[] args){
+        System.out.println("Tossing a coin...");
+        Random random = new Random();
+        int rand[] = {0, 0};
+        for(int i=0; i<3; i++){
+            int randomValue = random.nextInt(2);
+            if(randomValue == 0){
+                rand[0]++;
+                System.out.println("Round " + i + ": Heads");
+            }
+            else{
+                rand[1]++;
+                System.out.println("Round " + i + ": Tails");
+            }
+        }
+        System.out.println("Heads: " + rand[0]+ ", Tails: " + rand[1]);
+    }
+}
